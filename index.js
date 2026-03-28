037b9a3450aaaebf6c65be247137061b8a75d304
Rohit <rohitkumar@caplineservices.com>
2026-03-28 11:39:45 +0530

    ci test case

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+console.log('Setting up routes...');
 app.post('/add',(req,res) =>{
     const {num1, num2} = req.body;
     if(typeof num1 !== 'number' || typeof num2 !== 'number') {
